f2ae2b52f0483e81ff0375a7e9f0c50b1cdd9b95
MrGajera <sanjaygajera9876@gmail.com>
2025-01-23 20:01:48 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -68,23 +68,29 @@ document.querySelectorAll('.js-add-to-cart')
       let matchingitem;
 
       // we check if product already in the cart then increase Quantity
-      cart.forEach((item) =>{
-        if (productId===item.productName) {
+      cart.forEach((item) => {
+        if (productId === item.productId) {
           matchingitem = item;
         }
 
       });
 
-      if(matchingitem){
-        matchingitem.quantity+=1;
-      }else{
+      if (matchingitem) {
+        matchingitem.quantity += 1;
+      } else {
         cart.push({
-          productId:productId,
-          quantity :1
+          productId: productId,
+          quantity: 1
         });
       }
 
-      
-      console.log(cart);
+      let cartquantity = 0;
+      cart.forEach((item) => {
+        cartquantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML=cartquantity;
+
+
     });
   });
