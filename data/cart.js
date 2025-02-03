add3a95c5f748f902394b540d3caa415c839e613
MrGajera <sanjaygajera9876@gmail.com>
2025-02-03 17:08:26 +0530

    generate html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,21 +1,29 @@
-export const cart= [];
+// cart for save data of adding to the cart
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2,
+},
+{
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity:1,
+}];
 export function addtoCart(productId) {
-    let matchingitem;
-  
-    // we check if product already in the cart then increase Quantity
-    cart.forEach((cartitem) => {
-      if (productId === cartitem.productId) {
-        matchingitem = cartitem;
-      }
-  
-    });
-  
-    if (matchingitem) {
-      matchingitem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      });
+  let matchingitem;
+
+  // we check if product already in the cart then increase Quantity
+  cart.forEach((cartitem) => {
+    if (productId === cartitem.productId) {
+      matchingitem = cartitem;
     }
+
+  });
+
+  if (matchingitem) {
+    matchingitem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
   }
+}
