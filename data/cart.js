af7b92002687f6b6805ae977a6bf525cbac23e75
MrGajera <sanjaygajera9876@gmail.com>
2025-02-09 11:48:56 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,24 @@
 // cart for save data of adding to the cart
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-},
-{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+  }];
+}
+
+
+
+
+function savetostorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
 export function addtoCart(productId) {
   let matchingitem;
 
@@ -26,15 +38,18 @@ export function addtoCart(productId) {
       quantity: 1
     });
   }
+  savetostorage();
 }
 
 export function removeFromcart(productId) {
   const newcart = [];
-  cart.forEach((cartitem)=>{
-    if(cartitem.productId!==productId){
+  cart.forEach((cartitem) => {
+    if (cartitem.productId !== productId) {
       newcart.push(cartitem);
     }
   });
 
   cart = newcart;
+  savetostorage();
+
 }
