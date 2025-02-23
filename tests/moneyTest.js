c1382c8c522cc4c62b4930a81f34a2969781c762
MrGajera <sanjaygajera9876@gmail.com>
2025-02-23 11:38:31 +0530

    Create Automated Test for format currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,27 @@
+import {formatcurrency } from '../scripts/utils/money.js';
+if(formatcurrency(2000)==='20.00'){
+    console.log('YES');
+}else{
+    console.log('NO');
+
+}
+
+if(formatcurrency(0)==='0.00'){
+    console.log('YES');
+}else{
+    console.log('NO');
+
+}
+if(formatcurrency(2095)==='20.95'){
+    console.log('YES');
+}else{
+    console.log('NO');
+
+}
+
+if(formatcurrency(2000.5)==='20.01'){
+    console.log('YES');
+}else{
+    console.log('NO');
+
+}
