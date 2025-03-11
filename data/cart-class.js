5b1fb8a3c7e89d490aacfe2b0918fd87dbcc2d1d
MrGajera <sanjaygajera9876@gmail.com>
2025-03-11 13:44:09 +0530

    make local storage key and load from storage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart {
     cartItems ;
-    localStorageKe;
+    #localStorageKey;
 
     constructor(localStorageKey) {
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
     }
 
-    loadFromStorage() {
-        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
         if (!this.cartItems) {
             this.cartItems = [
                 {
@@ -25,8 +25,9 @@ class Cart {
 
         }
     }
+    // # means private 
     savetostorage() {
-        localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
     }
     addtoCart(productId) {
         let matchingitem;
@@ -78,8 +79,6 @@ class Cart {
 
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
-
-
 
 console.log(cart);
 console.log(businessCart);
